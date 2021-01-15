d2075d88e40e4e1ea99f676929a70316fe0f70c5
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2021-01-15 18:49:45 +0200

    fixed compose

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,4 @@ import thunk from 'redux-thunk';
 
 export const store = createStore(rootReducer, compose(
   applyMiddleware(thunk),
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 ));
